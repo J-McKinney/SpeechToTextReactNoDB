54f9f8a2db35c99cb169b176b0f5b27ef2357473
Jesse McKinney <jessman51386@gmail.com>
2020-04-24 20:46:04 -0400

    yay

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Dictaphone from "./components/SpeechRecognition/Dictaphone";
-import Recorder from "./components/Recorder/Recorder";
 
 function App() {
   return (
     <div>
-      <Recorder />
       <Dictaphone />
     </div>
   );
